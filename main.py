d908783ffa141a862a8b32c95a2d41449dcec4b5
Akmal <magchuz@pm.me>
2021-01-26 14:47:29 +0700

    Add multi episode support
    This is stupid mechanism.

--- main.py
+++ main.py
@@ -4,50 +4,78 @@ import time
 
 import facebook
 import schedule
-
+import video_frames
+from pathlib import Path
+# Parse config.ini
 config = configparser.ConfigParser()
 config.read("config.ini")
-
+fps = int(config["framebot"]["fps"])
+interval = int(config["framebot"]["interval"])
 
 def main():
-    frame_dir = sorted(
+    frame_dirs = sorted(
         [f for f in os.listdir("./frames") if not f.startswith(".")],
         key=lambda f: f.lower(),
     )
-    if len(frame_dir) == 0:
-        raise ValueError("Frame folder is empty.")
 
-    # Save temporary data about amount of frame
-    with open("./tmp_data", "a+") as f:
-        f.seek(0)
-        filled = f.read(1)
-        if not filled:
-            total_frames = str(len(frame_dir))
-            f.write(total_frames)
-        else:
+    for episode, frame_dir in enumerate(frame_dirs):
+        # First folder is always the current episode
+        inframe_dir = sorted(
+            [f for f in os.listdir(f"./frames/{frame_dir}") if not f.startswith(".")],
+            key=lambda f: f.lower(),
+        )
+
+        # Empty folder means that episode is done
+        if len(inframe_dir) == 0:
+            os.remove("./tmp_data")
+            continue
+        
+        # New Episode
+        # Save temporary data about amount of frame
+        with open("./tmp_data", "a+") as f:
             f.seek(0)
-            total_frames = str(f.readline())
+            filled = f.read(1)
+            if not filled:
+                total_frames = str(len(inframe_dir))
+                f.write(total_frames)
+            else:
+                f.seek(0)
+                total_frames = str(f.readline())
+        break
 
-    return post(frame_dir[0], total_frames)
+    final_frame_dir = f"./frames/{frame_dir}/{inframe_dir[0]}"
+    return post(final_frame_dir, total_frames, episode)
 
 
-def post(frame_fname, total_frames):
+def post(frame_dir, total_frames, episode):
     fb_token = config["framebot"]["fb_token"]
     prefix = config["framebot"]["prefix"]
-    current_frame = f"frames/{frame_fname}"
-    current_frame_num = os.path.splitext(frame_fname)[0]
+    frame_fname = Path(frame_dir).name
+    current_frame_num = os.path.splitext(frame_fname)[0].strip("0")
 
     message = (
-        f"{prefix} | Episode 1 | Frame {current_frame_num} out of {str(total_frames)}"
+        f"{prefix} | Episode {episode} | Frame {current_frame_num} out of {str(total_frames)}"
     )
     graph = facebook.GraphAPI(fb_token)
-    graph.put_photo(image=open(current_frame, "rb"), message=message)
+    graph.put_photo(image=open(frame_dir, "rb"), message=message)
     print(f"Posted frame {current_frame_num} of {str(total_frames)} successfully!")
-    os.remove(current_frame)
+    os.remove(frame_dir)
+
+
+def init_frame():
+    videos_dir = sorted(
+        [f for f in os.listdir("./videos") if not f.startswith(".")],
+        key=lambda f: f.lower(),
+    )
+
+    for video in videos_dir:
+        video_frames.video_to_frames(
+            video_path=f"./videos/{video}", frames_dir="frames", overwrite=False, every=fps
+        )
 
 
 if __name__ == "__main__":
-    interval = config["framebot"]["interval"]
+    init_frame()
     schedule.every(interval).minutes.do(main).run()
     while True:
         schedule.run_pending()
---
+++ video_frames.py
@@ -0,0 +1,104 @@
+import cv2
+import os
+import numpy as np
+from decord import VideoReader
+from decord import cpu, gpu
+
+
+def extract_frames(video_path, frames_dir, overwrite=False, start=-1, end=-1, every=1):
+    """
+    Extract frames from a video using decord's VideoReader
+    :param video_path: path of the video
+    :param frames_dir: the directory to save the frames
+    :param overwrite: to overwrite frames that already exist?
+    :param start: start frame
+    :param end: end frame
+    :param every: frame spacing
+    :return: count of images saved
+    """
+
+    video_path = os.path.normpath(video_path)  # make the paths OS (Windows) compatible
+    frames_dir = os.path.normpath(frames_dir)  # make the paths OS (Windows) compatible
+
+    video_dir, video_filename = os.path.split(
+        video_path
+    )  # get the video path and filename from the path
+
+    assert os.path.exists(video_path)  # assert the video file exists
+
+    # load the VideoReader
+    vr = VideoReader(video_path, ctx=cpu(0))  # can set to cpu or gpu .. ctx=gpu(0)
+
+    if start < 0:  # if start isn't specified lets assume 0
+        start = 0
+    if end < 0:  # if end isn't specified assume the end of the video
+        end = len(vr)
+
+    frames_list = list(range(start, end, every))
+    saved_count = 0
+
+    if (
+        every > 25 and len(frames_list) < 1000
+    ):  # this is faster for every > 25 frames and can fit in memory
+        frames = vr.get_batch(frames_list).asnumpy()
+        for index, frame in zip(
+            frames_list, frames
+        ):  # lets loop through the frames until the end
+            save_path = os.path.join(
+                frames_dir, video_filename, "{:010d}.jpg".format(saved_count)
+            )  # create the save path
+            if (
+                not os.path.exists(save_path) or overwrite
+            ):  # if it doesn't exist or we want to overwrite anyways
+                cv2.imwrite(
+                    save_path, cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+                )  # save the extracted image
+                saved_count += 1  # increment our counter by one
+
+    else:  # this is faster for every <25 and consumes small memory
+        for index in range(start, end):  # lets loop through the frames until the end
+            frame = vr[index]  # read an image from the capture
+
+            if (
+                index % every == 0
+            ):  # if this is a frame we want to write out based on the 'every' argument
+                save_path = os.path.join(
+                    frames_dir, video_filename, "{:010d}.jpg".format(saved_count)
+                )  # create the save path
+                if (
+                    not os.path.exists(save_path) or overwrite
+                ):  # if it doesn't exist or we want to overwrite anyways
+                    cv2.imwrite(
+                        save_path, cv2.cvtColor(frame.asnumpy(), cv2.COLOR_RGB2BGR)
+                    )  # save the extracted image
+                    saved_count += 1  # increment our counter by one
+
+    return saved_count  # and return the count of the images we saved
+
+
+def video_to_frames(video_path, frames_dir, overwrite=False, every=1):
+    """
+    Extracts the frames from a video
+    :param video_path: path to the video
+    :param frames_dir: directory to save the frames
+    :param overwrite: overwrite frames if they exist?
+    :param every: extract every this many frames
+    :return: path to the directory where the frames were saved, or None if fails
+    """
+
+    video_path = os.path.normpath(video_path)  # make the paths OS (Windows) compatible
+    frames_dir = os.path.normpath(frames_dir)  # make the paths OS (Windows) compatible
+
+    video_dir, video_filename = os.path.split(
+        video_path
+    )  # get the video path and filename from the path
+
+    # make directory to save frames, its a sub dir in the frames_dir with the video name
+    os.makedirs(os.path.join(frames_dir, video_filename), exist_ok=True)
+
+    print("Extracting frames from {}".format(video_filename))
+
+    extract_frames(video_path, frames_dir, every=every)  # let's now extract the frames
+    return os.path.join(
+        frames_dir, video_filename
+    )  # when done return the directory containing the frames
